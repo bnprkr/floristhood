82fbf41ed53b03b9e92b6618f4dd332268db29a6
Ben <benparker03@gmail.com>
2022-06-21 20:47:08 +0100

    temp

--- frontend/src/components/Welcome/Welcome.js
+++ frontend/src/components/Welcome/Welcome.js
@@ -1,10 +1,11 @@
 import bouquetImage from '../../assets/flowers-main'
+import styles from './Welcome.module.css'
 
 function Welcome() {
   return (
     <div className="container">
-      <div className="welcome-image">
-        <img src={bouquetImage} alt="A bouquet of pink flowers in a vase" />
+      <div>
+        <img src={bouquetImage} alt="A bouquet of pink flowers in a vase" className={styles.welcomeImage}/>
       </div>
     </div>
   );

1349a3cb69f9d743d7697e4f6fe78dba44a14db5
Ben <benparker03@gmail.com>
2022-06-21 12:30:06 +0100

    temp

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <div className="menu">
+        
+      </div>
+      <div className="logo">
+
+      </div>
+      <div className="icons">
+
+      </div>
+    </header>
+  );
+}

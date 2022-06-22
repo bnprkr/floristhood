66b0b7a484c9a9a366e772bbcd9f0602b411735b
Ben <benparker03@gmail.com>
2022-06-22 09:25:23 +0100

    temp

--- frontend/src/components/Welcome/Welcome.js
+++ frontend/src/components/Welcome/Welcome.js
@@ -3,9 +3,12 @@ import styles from './Welcome.module.css'
 
 function Welcome() {
   return (
-    <div className="container">
-      <div>
-        <img src={bouquetImage} alt="A bouquet of pink flowers in a vase" className={styles.welcomeImage}/>
+    <div className={styles.container}>
+      <img src={bouquetImage} alt="A bouquet of pink flowers in a vase" className={styles.welcomeImage}/>
+      <div className={styles.welcomeMessage}>
+        <h1>Personalise your <br/>flower arrangement</h1>
+        <p>Nam eu rutrum mauris. Cras tempor porttitor augue mattis consectetur.</p>
+        <button>Order now</button>
       </div>
     </div>
   );

e96460361ddd82e4be751ffbbb320520d4c7ac18
Ben <benparker03@gmail.com>
2022-06-23 16:33:06 +0100

    temp

--- frontend/src/components/OrderForm/Navigation/Navigation.js
+++ frontend/src/components/OrderForm/Navigation/Navigation.js
@@ -2,22 +2,29 @@ import styles from "./Navigation.module.css";
 
 function Navigation({ page, setPage, first }) {
   return (
-    <div className="container">
-      <button
-        onClick={() => {
-          setPage(page + 1);
-        }}
-        className={first ? styles.hide : ""}
-      >
-        Back
-      </button>
-      <button
-        onClick={() => {
-          setPage(page + 1);
-        }}
-      >
-        Next
-      </button>
+    <div className={styles.container}>
+      <div className={styles.back}>
+        {!first && (
+          <button
+            onClick={() => {
+              setPage(page + 1);
+            }}
+            className={styles.backButton}
+          >
+            &lt; Back
+          </button>
+        )}
+      </div>
+      <div className={styles.next}>
+        <button
+          onClick={() => {
+            setPage(page + 1);
+          }}
+          className={styles.nextButton}
+        >
+          Next &gt;
+        </button>
+      </div>
     </div>
   );
 }

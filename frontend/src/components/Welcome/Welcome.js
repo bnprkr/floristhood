b509ad714f6a785f0005fb71ff3a389f26d37b53
Ben <benparker03@gmail.com>
2022-06-27 20:36:20 +0100

    edits to default p sizing and started on Search responsive

--- frontend/src/components/Welcome/Welcome.js
+++ frontend/src/components/Welcome/Welcome.js
@@ -17,9 +17,9 @@ function Welcome() {
           <br />
           flower arrangement
         </h1>
-        <p>
+        <h2>
           Nam eu rutrum mauris. Cras tempor porttitor augue mattis consectetur.
-        </p>
+        </h2>
         <Link to="/order" className={styles.button}>
           Order now
         </Link>

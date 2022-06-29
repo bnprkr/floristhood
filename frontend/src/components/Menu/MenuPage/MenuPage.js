291b0ae8d0ef87754a1df18cb3d23657f0507607
Ben <benparker03@gmail.com>
2022-06-29 09:13:55 +0100

    initial MenuPage

--- frontend/src/components/Menu/MenuPage/MenuPage.js
+++ frontend/src/components/Menu/MenuPage/MenuPage.js
@@ -3,7 +3,24 @@ import styles from "./MenuPage.module.css";
 function MenuPage() {
   return (
     <>
-      <div className={styles.container}>Menu here..</div>
+      <div className={styles.container}>
+        <div className={styles.menuLinks}>
+          <div className={styles.menuItem}>
+            <a href="#">HOME</a>
+          </div>
+          <div className={styles.menuItem}>
+            <a href="#">SHOP</a>
+          </div>
+          <div className={styles.menuItem}>
+            <a href="#">ABOUT US</a>
+          </div>
+          <div className={styles.menuItem}>
+            <a href="#">CONTACT</a>
+          </div>
+        </div>
+
+        <div className={styles.logo}>floristhood</div>
+      </div>
     </>
   );
 }

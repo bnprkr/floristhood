7ae4135b3dd69df5ddc687ef8671c6e049d98491
Ben <benparker03@gmail.com>
2022-06-29 11:05:42 +0100

    expanding search bar skeleton added

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,20 +1,34 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Search.module.css";
+import { useState } from "react";
 
 function Search() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className={styles.container}>
-      <p>
+      <p className={open ? styles.hide : ""}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
         tincidunt quis tortor at dignissim. Donec porttitor, lectus sit amet
         efficitur hendrerit, libero dui rhoncus.
       </p>
-      <button>
-        <FontAwesomeIcon
-          icon={faMagnifyingGlass}
-          className={styles.magnifyingGlass}
-        />
+      <button
+        className={styles.search}
+        onClick={!open ? () => setOpen(!open) : ""}
+      >
+        {open ? (
+          <FontAwesomeIcon
+            icon={faXmark}
+            className={styles.close}
+            onClick={() => setOpen(!open)}
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className={styles.magnifyingGlass}
+          />
+        )}
       </button>
     </div>
   );

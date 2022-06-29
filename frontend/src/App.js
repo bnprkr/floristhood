44461a96898a3b6b1733e6464e08a046c5f115d6
Ben <benparker03@gmail.com>
2022-06-29 08:22:55 +0100

    added skeleton for menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,13 @@ import NewStyles from "./components/NewStyles/NewStyles";
 import Footer from "./components/Footer/Footer";
 import { Switch, Route } from "react-router-dom";
 import OrderForm from "./components/OrderForm/OrderForm";
+import MenuPage from "./components/Menu/MenuPage/MenuPage";
 
 function App() {
   return (
     <>
       <Header />
+      <MenuPage />
       <Switch>
         <Route exact path="/">
           <Welcome />
---
+++ frontend/src/components/Menu/MenuPage/MenuPage.js
@@ -0,0 +1,11 @@
+import styles from "./MenuPage.module.css";
+
+function MenuPage() {
+  return (
+    <>
+      <div className={styles.container}>Menu here..</div>
+    </>
+  );
+}
+
+export default MenuPage;

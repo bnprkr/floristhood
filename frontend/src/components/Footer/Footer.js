1a6138f5b08d543aec275cea1337dfe77c23aee0
Ben <benparker03@gmail.com>
2022-06-30 15:22:55 +0100

    finished mobile views for main components

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,47 +7,6 @@ import {
 
 function Footer() {
   return (
-    // <div className={styles.container}>
-    //   <div className={styles.left}>
-    //     <div className={styles.logo}>floristhood</div>
-    //     <div className={styles.social}>
-    //       <FontAwesomeIcon icon={faFacebook} />
-    //       <FontAwesomeIcon icon={faInstagramSquare} />
-    //     </div>
-    //   </div>
-    //   <div className={styles.right}>
-    //     <div className={styles.firstMenu}>
-    //       <ul>
-    //         <li>Terms & Conditions</li>
-    //         <li>Delivery Details</li>
-    //         <li>Accessibility</li>
-    //         <li>Privacy Policy</li>
-    //       </ul>
-    //     </div>
-    //     <div className={styles.secondMenu}>
-    //       <ul>
-    //         <li>Cookie Policy</li>
-    //         <li>About Us</li>
-    //         <li>Contact</li>
-    //       </ul>
-    //     </div>
-    //     <div className={styles.contact}>
-    //       <div className={styles.address}>
-    //         <address>
-    //           29 Lorem ipsum dolor
-    //           <br />
-    //           Consectetur
-    //           <br />
-    //           London
-    //           <br />
-    //           SE1 1AA
-    //         </address>
-    //       </div>
-    //       <div className={styles.email}>info@floristhood.com</div>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className={styles.container}>
       <div className={`${styles.column} ${styles.logoCol}`}>
         <div className={styles.logo}>floristhood</div>

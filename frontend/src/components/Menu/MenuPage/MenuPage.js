479dc538325f7a8088c5e7c7e63ac41db1c16f58
Ben <benparker03@gmail.com>
2022-07-12 11:43:33 +0100

    removed unused links and added Links to links in use, inc close menu onClick; added link styling to non-link items

--- frontend/src/components/Menu/MenuPage/MenuPage.js
+++ frontend/src/components/Menu/MenuPage/MenuPage.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useRef } from "react";
 import { useOnClickOutside } from "../../hooks";
+import { Link } from "react-router-dom";
 
 function MenuPage({ open, setOpen }) {
   const node = useRef();
@@ -18,18 +19,14 @@ function MenuPage({ open, setOpen }) {
           <FontAwesomeIcon icon={faXmark} onClick={() => setOpen(!open)} />
         </div>
         <div className={styles.menuLinks}>
-          <div className={styles.menuItem}>
-            <a href="#">HOME</a>
+          <div className={styles.menuItem} onClick={() => setOpen(!open)}>
+            <Link to="/">HOME</Link>
           </div>
-          <div className={styles.menuItem}>
-            <a href="#">SHOP</a>
+          <div className={styles.menuItem} onClick={() => setOpen(!open)}>
+            <Link to="/order">SHOP</Link>
           </div>
-          <div className={styles.menuItem}>
-            <a href="#">ABOUT US</a>
-          </div>
-          <div className={styles.menuItem}>
-            <a href="#">CONTACT</a>
-          </div>
+          <div className={styles.menuItem}>ABOUT US</div>
+          <div className={styles.menuItem}>CONTACT</div>
         </div>
 
         <div className={styles.logo}>floristhood</div>

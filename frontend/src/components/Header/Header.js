2b0185033ee6a1568554925aa23a599c4985c1f7
Ben <benparker03@gmail.com>
2022-07-12 10:22:31 +0100

    changed logo to Link to /

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faCartShopping } from "@fortawesome/free-solid-svg-icons";
@@ -17,7 +18,9 @@ function Header({ open, setOpen }) {
         <div className={styles.menu}>
           <FontAwesomeIcon icon={faBars} onClick={() => setOpen(!open)} />
         </div>
-        <div className={styles.logo}>floristhood</div>
+        <Link to="/" className={styles.logo}>
+          floristhood
+        </Link>
         <div className={styles.icons}>
           <LoginFormModal showModal={showModal} setShowModal={setShowModal} />
           <UserButton user={sessionUser} setShowModal={setShowModal} />

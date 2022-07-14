1f46993a4ef30e3e867a75207912459e7d5820f5
Ben <benparker03@gmail.com>
2022-07-14 14:57:04 +0100

    changed NewStyles height to auto with % padding

--- frontend/src/components/NewStyles/NewStyles.js
+++ frontend/src/components/NewStyles/NewStyles.js
@@ -5,17 +5,19 @@ import Thumbnail from "./Thumbnail/Thumbnail";
 function NewStyles() {
   return (
     <div className={styles.container}>
-      <h1>New Styles</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-        tincidunt quis tortor at dignissim.
-      </p>
-      <div className={styles.thumbnailContainer}>
-        <div className={styles.thumbnail}>
-          <Thumbnail title="British Tulips" imageName={newThumb1} />
-        </div>
-        <div className={styles.thumbnail}>
-          <Thumbnail title="Hand-tied Flowers" imageName={newThumb2} />
+      <div className={styles.content}>
+        <h1>New Styles</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          tincidunt quis tortor at dignissim.
+        </p>
+        <div className={styles.thumbnailContainer}>
+          <div className={styles.thumbnail}>
+            <Thumbnail title="British Tulips" imageName={newThumb1} />
+          </div>
+          <div className={styles.thumbnail}>
+            <Thumbnail title="Hand-tied Flowers" imageName={newThumb2} />
+          </div>
         </div>
       </div>
     </div>
